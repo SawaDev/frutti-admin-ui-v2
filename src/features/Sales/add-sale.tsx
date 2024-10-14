cc80669cd516a518d7f5d8d06beead563be0d511
sardormahmudovbellissimo <sardor.mahmudov@bellissimo.uz>
2024-10-14 12:25:51 +0500

    scrool in add sale

--- src/features/Sales/add-sale.tsx
+++ src/features/Sales/add-sale.tsx
@@ -13,6 +13,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
@@ -125,7 +126,7 @@ const AddSale: FC<SheetType> = ({ open, setOpen }) => {
 
     createSale.mutateAsync(data).then(() => {
       form.reset({});
-      setOpen(false)
+      setOpen(false);
     });
   };
 
@@ -136,158 +137,168 @@ const AddSale: FC<SheetType> = ({ open, setOpen }) => {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetContent className="sm:max-w-[80vw]">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <CardHeader className="flex flex-row items-center justify-between">
-              <div>
-                <CardTitle>Yangi sotuvni kiritish</CardTitle>
-                <CardDescription>
-                  Bu yerda siz yangi sotuv ma'lumotlarni kiritishingiz mumkin
-                </CardDescription>
+        <ScrollArea className="z-[9999] h-[96vh]">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardHeader className="flex flex-row items-center justify-between">
+                <div>
+                  <CardTitle>Yangi sotuvni kiritish</CardTitle>
+                  <CardDescription>
+                    Bu yerda siz yangi sotuv ma'lumotlarni kiritishingiz mumkin
+                  </CardDescription>
+                </div>
+                <div>
+                  <Button
+                    type="submit"
+                    size="sm"
+                    variant="default"
+                    className="ml-3"
+                    disabled={createSale.isPending}
+                  >
+                    Saqlash
+                  </Button>
+                </div>
+              </CardHeader>
+              <div className="my-5 grid grid-cols-5 gap-4">
+                <FormSelect
+                  control={form.control}
+                  name="client_id"
+                  label={
+                    <div className="flex items-center justify-start gap-1 py-1">
+                      Klientni tanlang{" "}
+                      {client && (
+                        <Popover>
+                          <PopoverTrigger>
+                            <InfoIcon className="h-4 w-4" />
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <ul>
+                              <li className="space-x-2">
+                                <b>Ismi:</b>
+                                <i>{client.name}</i>
+                              </li>
+                              <li className="space-x-2">
+                                <b>Balans:</b>
+                                <i>
+                                  {formatNumberComma(client.balance)}{" "}
+                                  {client.currency === "USD"
+                                    ? "$"
+                                    : client.currency}
+                                </i>
+                              </li>
+                            </ul>
+                          </PopoverContent>
+                        </Popover>
+                      )}
+                    </div>
+                  }
+                  options={clients?.data.map((client) => ({
+                    label: client.name,
+                    value: client.id.toString(),
+                  }))}
+                />
+                <FormSelect
+                  control={form.control}
+                  name="wallet_id"
+                  label={
+                    <div className="flex items-center justify-start gap-1 py-1">
+                      Kassani tanlang{" "}
+                      {wallet && (
+                        <Popover>
+                          <PopoverTrigger>
+                            <InfoIcon className="h-4 w-4" />
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <ul>
+                              <li className="space-x-2">
+                                <b>Nomi:</b>
+                                <i>{wallet.name}</i>
+                              </li>
+                              <li className="space-x-2">
+                                <b>Balans:</b>
+                                <i>
+                                  {formatNumberComma(wallet.balance)}{" "}
+                                  {wallet.type === "dollar" ? "$" : wallet.type}
+                                </i>
+                              </li>
+                            </ul>
+                          </PopoverContent>
+                        </Popover>
+                      )}
+                    </div>
+                  }
+                  options={wallets?.data.map((wallet) => ({
+                    label: wallet.name,
+                    value: wallet.id.toString(),
+                  }))}
+                />
+                <FormInput
+                  control={form.control}
+                  name="payment_received"
+                  label="Qilingan to'lov"
+                  type="number"
+                />
+                <FormSelect
+                  control={form.control}
+                  name="transaction_type"
+                  label="To'lov turi"
+                  options={paymentMethodOptions}
+                />
+                <FormSelect
+                  control={form.control}
+                  name="is_free"
+                  label="Aksiyami?"
+                  options={questionOptions}
+                />
               </div>
-              <div>
-                <Button
-                  type="submit"
-                  size="sm"
-                  variant="default"
-                  className="ml-3"
-                  disabled={createSale.isPending}
-                >
-                  Saqlash
-                </Button>
-              </div>
-            </CardHeader>
-            <div className="my-5 grid grid-cols-5 gap-4">
-              <FormSelect
-                control={form.control}
-                name="client_id"
-                label={
-                  <div className="flex items-center justify-start gap-1 py-1">
-                    Klientni tanlang{" "}
-                    {client && (
-                      <Popover>
-                        <PopoverTrigger>
-                          <InfoIcon className="h-4 w-4" />
-                        </PopoverTrigger>
-                        <PopoverContent>
-                          <ul>
-                            <li className="space-x-2">
-                              <b>Ismi:</b>
-                              <i>{client.name}</i>
-                            </li>
-                            <li className="space-x-2">
-                              <b>Balans:</b>
-                              <i>{formatNumberComma(client.balance)} {client.currency === "USD" ? "$" : client.currency}</i>
-                            </li>
-                          </ul>
-                        </PopoverContent>
-                      </Popover>
-                    )}
-                  </div>
-                }
-                options={clients?.data.map((client) => ({
-                  label: client.name,
-                  value: client.id.toString(),
-                }))}
-              />
-              <FormSelect
-                control={form.control}
-                name="wallet_id"
-                label={
-                  <div className="flex items-center justify-start gap-1 py-1">
-                    Kassani tanlang{" "}
-                    {wallet && (
-                      <Popover>
-                        <PopoverTrigger>
-                          <InfoIcon className="h-4 w-4" />
-                        </PopoverTrigger>
-                        <PopoverContent>
-                          <ul>
-                            <li className="space-x-2">
-                              <b>Nomi:</b>
-                              <i>{wallet.name}</i>
-                            </li>
-                            <li className="space-x-2">
-                              <b>Balans:</b>
-                              <i>{formatNumberComma(wallet.balance)} {wallet.type === "dollar" ? "$" : wallet.type}</i>
-                            </li>
-                          </ul>
-                        </PopoverContent>
-                      </Popover>
-                    )}
-                  </div>
-                }
-                options={wallets?.data.map((wallet) => ({
-                  label: wallet.name,
-                  value: wallet.id.toString(),
-                }))}
-              />
-              <FormInput
-                control={form.control}
-                name="payment_received"
-                label="Qilingan to'lov"
-                type="number"
-              />
-              <FormSelect
-                control={form.control}
-                name="transaction_type"
-                label="To'lov turi"
-                options={paymentMethodOptions}
-              />
-              <FormSelect
-                control={form.control}
-                name="is_free"
-                label="Aksiyami?"
-                options={questionOptions}
-              />
-            </div>
-            <Card>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Mahsulotlar</TableHead>
-                    <TableHead>Qancha bor</TableHead>
-                    <TableHead>Narxi</TableHead>
-                    <TableHead>Narxi (dollarda)</TableHead>
-                    <TableHead>Yangilangan Narx</TableHead>
-                    <TableHead>Soni</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {products?.data.map((product, productIndex) => {
-                    return (
-                      <TableRow key={productIndex}>
-                        <TableCell>{product.name}</TableCell>
-                        <TableCell>{product.quantity}</TableCell>
-                        <TableCell>{product.price}</TableCell>
-                        <TableCell>{product.price_in_dollar}$</TableCell>
-                        <TableCell>
-                          <FormInput
-                            control={form.control}
-                            type="number"
-                            step={0.01}
-                            name={`products.${productIndex}.price`}
-                            min={0}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <FormInput
-                            control={form.control}
-                            type="number"
-                            step={0.01}
-                            name={`products.${productIndex}.quantity`}
-                            min={0}
-                            max={product.quantity}
-                          />
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </Card>
-          </form>
-        </Form>
+              <Card>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Mahsulotlar</TableHead>
+                      <TableHead>Qancha bor</TableHead>
+                      <TableHead>Narxi</TableHead>
+                      <TableHead>Narxi (dollarda)</TableHead>
+                      <TableHead>Yangilangan Narx</TableHead>
+                      <TableHead>Soni</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {products?.data.map((product, productIndex) => {
+                      return (
+                        <TableRow key={productIndex}>
+                          <TableCell>{product.name}</TableCell>
+                          <TableCell>{product.quantity}</TableCell>
+                          <TableCell>{product.price}</TableCell>
+                          <TableCell>{product.price_in_dollar}$</TableCell>
+                          <TableCell>
+                            <FormInput
+                              control={form.control}
+                              type="number"
+                              step={0.01}
+                              name={`products.${productIndex}.price`}
+                              min={0}
+                            />
+                          </TableCell>
+                          <TableCell>
+                            <FormInput
+                              control={form.control}
+                              type="number"
+                              step={0.01}
+                              name={`products.${productIndex}.quantity`}
+                              min={0}
+                              max={product.quantity}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </Card>
+            </form>
+          </Form>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
